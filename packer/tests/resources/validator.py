2101dcbdb554f00e1cfc092277c7d5ba9048828c
nir0s <nir36g@gmail.com>
2015-04-26 20:32:32 +0300

    clean up and prepared testing env

--- packer/tests/resources/validator.py
+++
@@ -1,3 +0,0 @@
-def validate(file_path):
-    # returning True is a must if using the validator, otherwise repex will throw an exception.  # NOQA
-    return False
---
+++ packer/tests/test_packer.py
@@ -0,0 +1,36 @@
+import packer
+
+import testtools
+import os
+
+PACKER_PATH = '/usr/bin/packer'
+TEST_RESOURCES_DIR = 'packer/tests/resources/'
+TEST_PACKERFILE = os.path.join(TEST_RESOURCES_DIR, 'packerfile.json')
+TEST_BAD_PACKERFILE = os.path.join(TEST_RESOURCES_DIR, 'badpackerfile.json')
+
+
+class TestBase(testtools.TestCase):
+
+    def test_build(self):
+        p = packer.Packer(TEST_PACKERFILE)
+        p.build()
+
+    def test_fix(self):
+        p = packer.Packer(TEST_PACKERFILE)
+        p.fix()
+
+    def test_inspect(self):
+        p = packer.Packer(TEST_PACKERFILE)
+        p.inspect()
+
+    def test_push(self):
+        p = packer.Packer(TEST_PACKERFILE)
+        p.push()
+
+    def test_validate(self):
+        p = packer.Packer(TEST_PACKERFILE)
+        p.validate()
+
+    def test_version(self):
+        p = packer.Packer(TEST_PACKERFILE)
+        p.version()
