aac7e98b06b88872487b9982fc468a8be6b154ae
James Fryman <james@fryman.io>
2015-08-21 07:55:50 -0500

    update multiple arguments support

--- packer/__init__.py
+++ packer/__init__.py
@@ -170,7 +170,7 @@ class Packer():
         elif self.only:
             self._add_opt('-only={0}'.format(self._joinc(self.only)))
         for var, value in self.vars.items():
-            self._add_opt('-var \'{0}={1}\''.format(var, value))
+            self._add_opt("-var='{0}={1}'".format(var, value))
         if self.vars_file:
             self._add_opt('-var-file={0}'.format(self.vars_file))
 
